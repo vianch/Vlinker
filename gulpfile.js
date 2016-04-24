536a84374197d380532489e7b84a5ada5940d575
Victor Chavarro <info@vianch.com>
2016-04-23 19:21:23 -0500

    added spinner loader

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,23 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function () {
-    return gulp.src('./app/assets/styles/vlinker/gui.scss')
+    return gulp.src('./app/assets/styles/loader/loader.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./app/assets/styles/vlinker/gui.scss', ['sass']);
+  gulp.watch('./app/assets/styles/loader/loader.scss', ['sass']);
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('./build/css/loader.css')
+        .pipe(cleanCSS({debug: true}, function(details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+        }))
+        .pipe(gulp.dest('./build/css'));
 });
